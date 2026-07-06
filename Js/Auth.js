67e3646bb77ad3e0a494607e77a01fd39f9007ea
rsemon95-afk <rsemon95@gmail.com>
2026-07-07 02:28:59 +0600

    Update Auth.js

--- Js/Auth.js
+++ Js/Auth.js
@@ -1,1 +1,16 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+window.login = function () {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const msg = document.getElementById("msg");
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      msg.innerText = error.message;
+    });
+};
